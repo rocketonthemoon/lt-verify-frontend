4be9cc6c8d828e239e4c99a2a28c5aa69e099287
Vartotojas <alonaammu123@gmail.com>
2026-05-27 08:24:49 +0300

    fix api base

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000/api'
+const API_BASE = import.meta.env.VITE_API_BASE
 
 async function request(path, opts = {}) {
   const { headers: extraHeaders, ...restOpts } = opts
